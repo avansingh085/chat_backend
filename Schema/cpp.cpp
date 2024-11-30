088d1bb957efa0b3bb4e8fd48306a835409e0d29
Avan Singh <avansingh085@gmail.com>
2024-11-30 15:15:32 +0530

    ok

--- Schema/cpp.cpp
+++ Schema/cpp.cpp
@@ -1,43 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
 
 int main() {
-   ll t;
-   cin>>t;
-   while(t--)
-   {
-      ll b,a;
-      cin>>a>>b;
-      ll x,y;
-      cin>>x>>y;
-      bool flag=false;
-    //case 1   left side common
-    if(((y+b*2)<=a&&y>=b&&((b+1)/2)>=x)||((a-x-b)>=b&&(a-y-b)>=b))
-    {
-        flag=true;
-    }
-    //case 2 right side common 
-    if(((y+b*2)<=a&&y>=b&&((b+1)/2)>=(a-x-b))||(x>=b&&y>=b))
-    {
-        flag=true;
-    }
-    // case 3 top side common
-    if((((x+b*2)<=a&&x>=b&&((b+1)/2)>=y))||(((a-x-b)>=b&&y>=b)))
-    {
-        flag=true;
-    }
-   // case 3 bottom side common
-    if((((x+b*2)<=a&&x>=b&&((b+1)/2)>=(b-y-a)))||(((a-y-b)>=b&&x>=b)))
-    {
-        flag=true;
-    }
-    if(flag)
-    cout<<"YES"<<endl;
-    else
-    cout<<"NO"<<endl;
-      
-   }
+	int n;
+	cin>>n;
+	vector<vector<char>>s(n,vector<char>(n,'A'));
+	for(int i=0;i<n;i++)
+	{
+	    for(int j=0;j<n;j++)
+	    {
+	    cin>>s[i][j];
+	    }
+	}
+	int q;
+	cin>>q;
+	while(q--)
+	{
+	    int r,c,k;
+	    cin>>r>>c>>k;
+	    int cnt=0;
+	    while(k>1)
+	    {
+	    vector<vector<int>>v;
+	    cnt++;
+	    for(int i=c;i<(c+k);i++)
+	    {
+	        v.push_back({r,i,s[r][i]-'A'});
+	    }
+	    
+	     for(int i=r+1;i<(r+k);i++)
+	    {
+	        v.push_back({i,c+k-1,s[i][c+k-1]-'A'});
+	    }
+	    for(int i=c+k-2;i>=c;i--)
+	    {
+	        v.push_back({r+k-1,i,s[r+k-1][i]-'A'});
+	    }
+	    for(int i=r+k-2;i>r;i--)
+	    {
+	        v.push_back({i,c,s[i][c]-'A'});
+	    }
+	   // cout<<v.size()<<endl;
+	   int sz=v.size();
+	    for(int i=0;i<sz;i++)
+         {
+             if(cnt%2)
+             {
+             int num=v[(i+cnt)%sz][2];
+             num--;
+             if(num<0)
+             num=25;
+             s[v[i][0]][v[i][1]]=(char)('A'+num);
+             }
+             else
+             {
+                  int num=v[(i-cnt+sz)%sz][2];
+             num++;
+             num=num%26;
+             s[v[i][0]][v[i][1]]=(char)('A'+num);
+             }
+             //cout<<v[i][0]<<" "<<v[i][1]<<" "<<v[i][2]<<endl;
+         }
+         k-=2;
+         r++;
+         c++;
+	    }
+//          for(auto i : s)
+// 	{
+// 	    for(auto j : i)
+// 	   {
+// 	        cout<<j<<"  ";
+// 	    }
+// 	    cout<<endl;
+// 	} 
+	}
+	for(auto i : s)
+	{
+	    for(auto j : i)
+	   {
+	        cout<<j<<"";
+	    }
+	}
+	return 0;
 
-    return 0;
 }
--- Schema/database.js
+++ Schema/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Chat');
+        await mongoose.connect("mongodb+srv://avansingh085:SbhUyHjWETMpJWUN@cluster0.tyyrk.mongodb.net/Chat");
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
