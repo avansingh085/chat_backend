741f2ca42dcf6810ae485b2aeadcbb29a69b38bc
Avan Singh <avansingh085@gmail.com>
2025-04-27 10:27:56 +0530

    update

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const http = require('http');
 const app = require('./app');
 const socketHandler = require('./socket/socketServer');
 const PORT = 3001;
-const server = http.createServer(app);
+app.use(cors({ origin: "*"}));
+const options = {
+    key: fs.readFileSync("server-key.pem"),
+    cert: fs.readFileSync("server-cert.pem"),
+};
+const server = http.createServer(options,app);
+const io = new Server(server, {
+    cors: { origin: "*" },
+});
 socketHandler(server);
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
